ff66fae349ffccc348dcdf4e62b06d5c20ca999d
felipe.tavares <felipe.tavares@arce.ce.gov.br>
2016-08-23 15:34:18 -0300

    modelagem

--- beer/src/main/java/model/TamanhoCerveja.java
+++ beer/src/main/java/model/TamanhoCerveja.java
@@ -1,7 +1,45 @@
 package model;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * Created by felipe.tavares on 23/08/16.
  */
-public class TamanhoCerveja {
+
+@Entity
+@Table(name = "tb_tamanho_cerveja", schema = "beer")
+@AttributeOverride(name = "id", column = @Column(name = "id_tam_cerveja"))
+@SequenceGenerator(name = "SQ_GENERATOR", sequenceName = "beer.tam_cerveja_id_seq", allocationSize = 1)
+public class TamanhoCerveja extends AbstractModel<Long> implements Serializable {
+
+
+    @NotNull
+    @Column(name = "tam_cerveja_nome")
+    private String nome;
+
+    @Lob
+    @Column(name = "tam_cerveja_qtd")
+    private Double quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Double quantidade) {
+        this.quantidade = quantidade;
+    }
 }
--- beer/src/main/java/model/TipoCerveja.java
+++ beer/src/main/java/model/TipoCerveja.java
@@ -1,7 +1,30 @@
 package model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
 /**
  * Created by felipe.tavares on 23/08/16.
  */
-public class TipoCerveja {
+
+@Entity
+@Table(name = "tb_tipo_cerveja", schema = "beer")
+@AttributeOverride(name = "id", column = @Column(name = "id_tp_cerveja"))
+@SequenceGenerator(name = "SQ_GENERATOR", sequenceName = "beer.tp_cerveja_id_seq", allocationSize = 1)
+public class TipoCerveja extends AbstractModel<Long> implements Serializable {
+
+
+    @NotNull
+    @Column(name = "tp_cerveja_nome")
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 }
